3806c85dc73bc2948a99ddbe520e691132456562
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-04-24 22:11:51 +0200

    sum & compare

--- 1.0.js
+++ 1.0.js
@@ -1,3 +1,4 @@
+// Sum two numbers
 // Write a function that takes two numbers (a and b) as argument
 // Sum a and b
 // Return the result
@@ -5,4 +6,21 @@ function addingTwoNums (a, b) {
     let sum = a + b;
     return sum;
 }
-console.log(addingTwoNums(5, 25));
+console.log(addingTwoNums(5, 25)); // 30
+
+// Comparison operators, strict equality
+// Write a function that takes two values, say a and b, as arguments
+// Return true if the two values are equal and of the same type
+function comparisonOp (a, b) {
+    if(a===b) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(comparisonOp(5, 10)); //false
+console.log(comparisonOp(5, '5')); //false
+console.log(comparisonOp(5, 5)); //true
+console.log(comparisonOp("A", "B")); //false
+
+
