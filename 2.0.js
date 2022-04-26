64e84dc454b8e4070e9999ef68a27222e8517872
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-04-26 23:18:17 +0200

    Get last n elements of an array

--- 2.0.js
+++ 2.0.js
@@ -17,9 +17,23 @@ function removeElements (a) {
     let length = a.length+1;
     let newArr = a.slice(3,length); 
     return newArr;
-
-
 }
 console.log(removeElements([1,2,3,4]));//4
 console.log(removeElements([5,4,3,2,1,0]));//2,1,0
 console.log(removeElements([99,1,1]));//" "
+
+// Get last n elements of an array
+// Write a function that takes an array (a) as argument
+// Extract the last 3 elements of a
+// Return the resulting array
+function lastThreeElements (a) {
+    let end = a.length;
+    let start = a.length - 3;
+    let newArr = a.slice(start,end); 
+    return newArr;
+}
+
+console.log(lastThreeElements([1,2,3,4]));//2, 3, 4
+console.log(lastThreeElements([5,4,3,2,1,0]));//2, 3, 4
+
+
