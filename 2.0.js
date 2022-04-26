5c4f2cefa572a8c3cdf902d69f0ba6ba4d16bc37
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-04-27 00:23:39 +0200

    Check if all array elements are equal

--- 2.0.js
+++ 2.0.js
@@ -133,5 +133,26 @@ function longestString (a) {
 } 
 console.log(longestString(['help', 'me'])); //helt
 
+// Check if all array elements are equal
+// Write a function that takes an array as argument
+// It should return true if all elements in the array are equal
+// It should return false otherwise
+function checkEqual (a) {
+    var first = a[0];
+    for (let i = 1; i < a.length; i++) {
+      if (first !== a[i]) {
+        return false;
+      }
+    }
+    return true;
+}
+console.log(checkEqual(([true, true, true, true])));//true
+console.log(checkEqual(([1,1,1,2])));//false
+
+
+
+
+
+
 
 
