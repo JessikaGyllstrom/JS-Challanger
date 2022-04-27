22ca3ede0bb177e87414e4d813eacdc799321d7e
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-04-27 21:58:10 +0200

    Return the number of days between two dates

--- 3.0.js
+++ 3.0.js
@@ -4,9 +4,9 @@
 // It should return true if the dates are equal
 // It should return false otherwise
 function checkIfDateEqual (a, b) {
-    if(a > b){
+    if(a > b) {
         return false;
-    } else if(a < b){
+    } else if(a < b) {
         return false;
     } else {
         return true;
@@ -14,3 +14,16 @@ function checkIfDateEqual (a, b) {
 }
 console.log(checkIfDateEqual(new Date('2000/01/01 08:00:00'), new Date('2000/01/01 08:45:00')));//fasle
 console.log(checkIfDateEqual(new Date('2000/01/01 08:00:00'), new Date('2000/01/01 08:00:00')));//true
+
+// Return the number of days between two dates
+// Write a function that takes two date instances as argument
+// It should return the number of days that lies between those dates
+function numOfDays (a, b) {
+    console.log(a);
+    const oneDay = 24 * 60 * 60 * 1000; 
+    const diffDays = Math.round(Math.abs((a - b) / oneDay));
+    console.log(diffDays);
+}
+console.log(numOfDays(new Date('2020-06-11'), new Date('2020-06-01')));
+console.log(numOfDays(new Date('2000-01-01'), new Date('2020-06-01')));
+
